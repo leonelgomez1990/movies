246965e2d31e176abff8e7c9e28cefbe2c57eab1
leonelgomez1990 <leonelgomez@live.com.ar>
2022-03-12 12:33:52 -0300

    hilt configuration

---
+++ app/src/main/java/com/lgomez/movies/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.lgomez.movies.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
--- app/src/main/java/com/lgomez/movies/ui/MainActivity.kt
+++ app/src/main/java/com/lgomez/movies/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.lgomez.movies
+package com.lgomez.movies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lgomez.movies.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
